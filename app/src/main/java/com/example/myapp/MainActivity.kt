6cf971b3f543e9cc3d3dbf8a02a64da9b8359a41
GFavaz <abashidze-giorgi@gzebi.com>
2021-02-19 22:51:16 +0400

    ...

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.asasd)
+    }
+
+    override fun onResume() {
+        super.onResume()
         setContentView(R.layout.login_activity)
     }
 }
