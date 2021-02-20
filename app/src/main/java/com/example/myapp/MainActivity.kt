082b678a9d29b446278777e959aa6cadbf51625f
GFavaz <abashidze-giorgi@gzebi.com>
2021-02-20 14:05:33 +0400

    ...

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        setContentView(R.layout.login_activity)
     }
 
     fun button_decr_listener(view: View) {
