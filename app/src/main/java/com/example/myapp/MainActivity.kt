9b611d55a74f8d7338d9646fdf59a471fc812b6e
GFavaz <abashidze-giorgi@gzebi.com>
2021-02-20 12:11:21 +0400

    ...

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.myapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-abstract class MainActivity : AppCompatActivity() {
-
-    val but1 = R.id.button_decr
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
     }
-}
 
+}
