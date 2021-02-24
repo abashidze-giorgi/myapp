7ff03222f8d41d526d3daa1c00fea9a56b395f6f
GFavaz <abashidze-giorgi@gzebi.com>
2021-02-24 17:10:59 +0400

    ...

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.myapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
+
     }
 
     override fun onResume() {
@@ -30,10 +34,25 @@ class MainActivity : AppCompatActivity() {
         val numbersMap = mapOf("decr" to -1, "incr" to 1)
         val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
         val progress_bar_text_view = findViewById<TextView>(R.id.progress_bar_text_view)
+
         if (progressBar.progress <= 95 || progressBar.progress >= 5) {
             progressBar.progress += 5* numbersMap[string]!!.toInt()
             progress_bar_text_view.text = progressBar.progress.toString()
         }
+    }
+
+    fun logActRegisterButtonID_Click(view: View) {
+        val userName = findViewById<TextView>(R.id.logActUserNameTextEdit).text.toString()
+        openRegistrationActivity(view, userName)
+        
+    }
+
+    private fun openRegistrationActivity(view: View, userName: String) {
+        val intent = Intent(this, registration_activity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
     }
 }
 
