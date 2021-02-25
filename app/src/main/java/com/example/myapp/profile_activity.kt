5b2a5635151ab18d2be5f78ea58e7a40c6d60bb0
GFavaz <abashidze-giorgi@gzebi.com>
2021-02-25 13:54:10 +0400

    ...

---
+++ app/src/main/java/com/example/myapp/profile_activity.kt
@@ -0,0 +1,11 @@
+package com.example.myapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class profile_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.profile_activity)
+    }
+}
